a2885fdb813dc4be702e0c431c99fc66926a542a
Maxim Pribytkov <pribytkovmaksim919@gmail.com>
2025-11-10 12:38:41 +0500

    feat(backend): финальное редактирование, добавление ссылки на проект

--- frontend/src/utils/constants.ts
+++ frontend/src/utils/constants.ts
@@ -1,2 +1,2 @@
-export const API_URL = 'https://zloydoberman.nomorepartiessbs.ru/api/afisha';
-export const CDN_URL = 'https://zloydoberman.nomorepartiessbs.ru/content/afisha';
+export const API_URL = 'http://zloydoberman.nomorepartiessbs.ru/api/afisha';
+export const CDN_URL = 'http://zloydoberman.nomorepartiessbs.ru/content/afisha';
